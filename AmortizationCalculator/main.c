bd2551b8d17a8049f4247dcd0d35c65000af4ee2
schoate7 <schoate@me.com>
2021-04-03 01:47:22 -0400

    Initial Commit

---
+++ AmortizationCalculator/main.c
@@ -0,0 +1,14 @@
+//
+//  main.c
+//  AmortizationCalculator
+//
+//  Created by Stephen Choate on 4/3/21.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
